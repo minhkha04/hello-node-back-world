bb8d896c7bc7157a84aa353b0c80fa5566297431
Le Nguyen Minh Kha <lenguyenminhkha1606@gmail.com>
2025-09-01 23:26:42 +0700

    Refactor Prisma schema and enhance API endpoints for food management

--- handle-on-prisma/index.js
+++ handle-on-prisma/index.js
@@ -7,13 +7,44 @@
 // yarn add prisma @prisma/client
 // yarn prisma init
 // database first: yarn prisma db pull
-// code first: yarn prisma first
+// code first: yarn prisma db push
+// yarn prisma generate to update client
+
 import express from "express"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 app.listen(8080)
 
-app.get("/", (req, res) => {
-  res.send("Hello World")
+app.get("/", async (req, res) => {
+  let data = await prisma.foods.findMany({
+    include: {
+      food_types: true
+    },
+  })
+  res.json(data)
+})
+
+app.post("/", async (req, res) => {
+  let { name, food_type_id } = req.body
+
+  let data = await prisma.foods.create({
+    data: {
+      name, food_type_id
+    }
+  })
+  res.json(data)
+})
+
+app.put("/:id", async (req, res) => {
+  let { id } = req.params
+  let { name, food_type_id } = req.body
+  let data = await prisma.foods.update({
+    where: { id: Number(id) },
+    data: { name, food_type_id }
+  })
+  res.json(data)
 })
